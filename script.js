96e80143e885cbffe183491b2ed1e455dbcf469b
Samuel Sellberg <samuel.selle@hotmail.com>
2023-12-17 02:54:36 +0100

    new google test

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+document.addEventListener('scroll',parallax(evt));
+
+function parallax(evt) {
+    var elements = document.getElementsByClassName('parallax');
+    for (let i=0;i<elements.length;i++) {
+        null;
+    }
+}
+
