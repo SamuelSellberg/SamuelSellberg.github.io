c76e3d49d4f420533f157e51ed302b302dc3bab0
Samuel Sellberg <samuel.selle@hotmail.com>
2023-12-18 01:10:08 +0100

    more holoscreen pictures

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ document.addEventListener('scroll',parallax(evt));
 function parallax(evt) {
     var elements = document.getElementsByClassName('parallax');
     for (let i=0;i<elements.length;i++) {
-        null;
+        elements[i].setAttribute('style',"object-position: 0 100%;")
+        console.log("Scrolled")
     }
 }
 
+document.getElementById("testing").innerHTML = "Yes";
+
+console.log("Testing");
