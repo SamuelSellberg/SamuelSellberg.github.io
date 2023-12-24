5b325b08d6a99a17d9e6c84ef23974981a45abf7
Samuel Sellberg <samuel.selle@hotmail.com>
2023-12-24 02:32:11 +0100

    updated parallax

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
-function parallax(position) {
+function parallax() {
     var windowHeight = window.innerHeight;
     var elements = document.getElementsByClassName('parallax');
     for (let i=0;i<elements.length;i++) {
-        var rect = elements[i].getBoundingClientRect();
-        var compensation = 0;
+        var element = elements[i];
+        var rect = element.getBoundingClientRect();
         var offset = 1;
         if (rect.top <= windowHeight && rect.bottom >= 0) {
-            compensation = 0;
-            if ((rect.top + position) < windowHeight) {
-                compensation += windowHeight - rect.top - position;
-            }
-            offset = (rect.bottom/(rect.bottom - (rect.top + compensation) + windowHeight))*100;
-            //console.log(offset);
-            elements[i].setAttribute('style',"object-position: 0 " + offset + "%;");
+            let upper = parseFloat(element.dataset.upperoffset);
+            let lower = parseFloat(element.dataset.loweroffset);
+            offset = (((rect.bottom - upper)/(windowHeight + rect.bottom - rect.top - upper - lower))*100).toFixed(2);
+            element.setAttribute('style',"object-position: 0 " + offset + "%;");
         }
-
-        
-        
     }
 }
 
 function scrollToCenterImage(imageID) {
     let rect = document.getElementById(imageID).getBoundingClientRect();
     let windowHeight = window.innerHeight;
-    window.scrollTo(0,(rect.top + rect.bottom - windowHeight)/2);
+    window.scrollTo(0,(rect.top + rect.bottom + 2*window.scrollY - windowHeight)/2);
 }
 
 document.addEventListener("scroll", (event) => {
     //console.log(window.scrollY);
-    parallax(window.scrollY);
+    parallax();
 })
 
-
+parallax();
 // window.onload = scrollToCenterImage("focusImage");
